d467ce1698e7da820e36ccae2d57766387c7f441
vmirsadraee <vmirsadraee@gmail.com>
2026-06-05 22:03:27 +0330

    initial commit

--- src/Pages/api.js
+++ src/Pages/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "consultant2-peach.vercel.app"
+  baseURL: "https://consultant2-peach.vercel.app"
 });
 
 export default api;
-//  http://localhost:5000
+//  http://localhost:5000  https://consultant2-peach.vercel.app/tablea1403/befor
