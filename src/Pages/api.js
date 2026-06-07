446f157adc57f0c9541006656840e83fa75ef120
vmirsadraee <vmirsadraee@gmail.com>
2026-06-07 14:43:51 +0330

    Update api.js

--- src/Pages/api.js
+++ src/Pages/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+// آدرس پایه شما
+export const API_URL = "https://consultant2-b.vercel.app";
+
 const api = axios.create({
-  baseURL: "https://consultant2-b.vercel.app/"
+  baseURL: API_URL
 });
 
 export default api;
-//  http://localhost:5000  https://consultant2-peach.vercel.app/tablea1403/befor
