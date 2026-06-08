a7a07d866df9269064d1558637cc2d9cfb4afd86
vmirsadraee <vmirsadraee@gmail.com>
2026-06-08 19:05:36 +0330

    Update Bill.page.jsx

--- src/Pages/Bill.page.jsx
+++ src/Pages/Bill.page.jsx
@@ -71,7 +71,7 @@ const sections = [
   <div className="d-flex gap-2">
     <button
       className="btn btn-success"
-      onClick={() =>window.open(`${API2}`)}
+      onClick={() =>window.open("https://consultant2-b.vercel.app/download-excel")}
 
     >
       دانلود فایل
