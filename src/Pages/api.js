925284207d37ee06e0c91b175736d85fe54f3eda
vmirsadraee <vmirsadraee@gmail.com>
2026-06-08 18:52:21 +0330

    Update api.js

--- src/Pages/api.js
+++ src/Pages/api.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-// آدرس پایه شما
+// آدرس پایه API
 export const API_URL = "https://consultant2-b.vercel.app";
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: API_URL
 });
 
-export default api;
-// آدرس پایه شما
+// آدرس دانلود اکسل
 export const API_URL2 = "https://consultant2-b.vercel.app/download-excel";
 
-const api2 = axios.create({
+export const api2 = axios.create({
   baseURL: API_URL2
 });
 
-export default api2;
